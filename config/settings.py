5d7a3d4f99e6eb232acf83382c984b99d3d843f8
Adil S <forever@nzironeed.ru>
2023-08-04 00:44:49 +0500

    add users and update spa_app/models.py

--- config/settings.py
+++ config/settings.py
@@ -138,4 +138,4 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
-#AUTH_USER_MODEL = 'users.User'
+AUTH_USER_MODEL = 'users.User'
--- spa_app/models.py
+++ spa_app/models.py
@@ -1,3 +1,20 @@
 from django.db import models
+from config.settings import AUTH_USER_MODEL
+from django.utils.timezone import now
 
-# Create your models here.
+NULLABLE = {'null': True, 'blank': True}
+
+
+class Habit(models.Model):
+    user = models.ForeignKey('AUTH_USER_MODEL', on_delete=models.CASCADE, verbose_name='user', **NULLABLE)
+    place = models.CharField(max_length=100, verbose_name='place')
+    time = models.TimeField(default=now, verbose_name='habit starts')
+    action = models.TextField(verbose_name='action')
+    nice_habit = models.BooleanField(default=False, verbose_name='is nice habit')
+    linked_habit = models.ForeignKey('self', on_delete=models.CASCADE,
+                                     verbose_name='linked habit (only for nice habit)', **NULLABLE)
+    frequency = models.TextChoices() #ZAPOLNI
+    reward = models.CharField(max_length=100, verbose_name='reward (only for default habit)', **NULLABLE)
+    duration = models.IntegerField(**NULLABLE, verbose_name='duration')
+    published = models.BooleanField(default=False, verbose_name='is published')
+
