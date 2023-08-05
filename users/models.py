2a6496319df1b076bc35605f7a549865a9b26f06
Adil S <forever@nzironeed.ru>
2023-08-05 17:31:50 +0500

    update classe User

--- users/models.py
+++ users/models.py
@@ -3,7 +3,16 @@ from django.db import models
 
 
 class User(AbstractUser):
+    username = None
+
+    email = models.EmailField(unique=True, max_length=50, verbose_name='email')
+
+    USERNAME_FIELD = "email"
+    REQUIRED_FIELDS = []
 
     class Meta:
         verbose_name = 'User'
         verbose_name_plural = 'Users'
+
+    def __str__(self):
+        return self.email
