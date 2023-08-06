5aa50c026bdbe7892bd3397b7379ab8a062ad339
Adil S <forever@nzironeed.ru>
2023-08-06 20:01:23 +0500

    small change in urls.py (path) for spa_app

--- spa_app/urls.py
+++ spa_app/urls.py
@@ -9,10 +9,10 @@ app_name = SpaAppConfig.name
 
 
 urlpatterns = [
-    path('spa/habits/publish/', HabitPublishListView.as_view(), name='habit_publish'),
-    path('spa/habits/my/', HabitPrivateListView.as_view(), name='habit_private'),
-    path('spa/habits/update/<int:pk>/', HabitUpdateView.as_view(), name='habit_update'),
-    path('spa/habits/retrieve/<int:pk>/', HabitRetrieveView.as_view(), name='habit_retrieve'),
-    path('spa/habits/destroy/<int:pk>/', HabitDestroyView.as_view(), name='habit_destroy'),
-    path('spa/habits/create/', HabitUpdateView.as_view(), name='habit_create'),
+    path('habits/publish/', HabitPublishListView.as_view(), name='habit_publish'),
+    path('habits/my/', HabitPrivateListView.as_view(), name='habit_private'),
+    path('habits/update/<int:pk>/', HabitUpdateView.as_view(), name='habit_update'),
+    path('habits/retrieve/<int:pk>/', HabitRetrieveView.as_view(), name='habit_retrieve'),
+    path('habits/destroy/<int:pk>/', HabitDestroyView.as_view(), name='habit_destroy'),
+    path('habits/create/', HabitUpdateView.as_view(), name='habit_create'),
 ]
