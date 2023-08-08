e533ee4c61f7eafac58a82a69340dceb7c0735eb
Adil S <forever@nzironeed.ru>
2023-08-09 00:22:49 +0500

    update permissions.py

--- permissions.py
+++ permissions.py
@@ -1,9 +1,10 @@
-from rest_framework.permissions import BasePermission
-
-from users.models import User
+from rest_framework.permissions import BasePermission, SAFE_METHODS
 
 
 class IsOwner(BasePermission):
 
     def has_object_permission(self, request, view, obj):
+        if request.method in SAFE_METHODS:
+            return True
+
         return request.user == obj.user
