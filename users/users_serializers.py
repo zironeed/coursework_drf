08d3e22bb0a70cb98460954d8ff2511c5eea7fa2
Adil S <forever@nzironeed.ru>
2023-08-09 00:20:07 +0500

    update users_serializers.py

--- users/users_serializers.py
+++ users/users_serializers.py
@@ -10,13 +10,27 @@ class UserSerializer(serializers.ModelSerializer):
 
 
 class UserRegisterSerializer(serializers.ModelSerializer):
-    password = serializers.CharField(write_only=True)
+    password2 = serializers.CharField()
 
     class Meta:
         model = User
-        fields = ('email', 'password', 'tg_name',)
+        fields = ('email', 'password1', 'password2', 'tg_name',)
 
-    def create(self, validated_data):
-        user = User.objects.create_user(**validated_data)
+    def save(self, **kwargs):
+        user = User(
+            email=self.validated_data['email'],
+            tg_name=self.validated_data['tg_name'],
+            fist_name=self.validated_data['email'],
+            last_name=self.validated_data['email'],
+            is_superuser=False,
+            is_stuff=False,
+            is_active=True
+        )
+
+        if self.validated_data['password1'] != self.validated_data['password2']:
+            raise serializers.ValidationError('Passwords do not match')
+
+        user.set_password(self.validated_data['password1'])
+        user.save()
 
         return user
