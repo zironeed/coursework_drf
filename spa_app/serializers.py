d9ea2d84f620264c4f69bd8a0b66e81c46ff2485
Adil S <forever@nzironeed.ru>
2023-08-13 21:07:04 +0500

    add tests.py

--- spa_app/serializers.py
+++ spa_app/serializers.py
@@ -1,14 +1,31 @@
 from rest_framework import serializers
 
 from spa_app.models import Habit
-from spa_app.spa_validators import habit_validator
 
 
-class HabitSerializer(serializers.BaseSerializer):
-    nice_habit = serializers.BooleanField(validators=[habit_validator])
-    linked_habit = serializers.CharField(validators=[habit_validator])
-    reward = serializers.CharField(validators=[habit_validator])
-    duration = serializers.IntegerField(validators=[habit_validator])
+class HabitSerializer(serializers.ModelSerializer):
+
+    def create(self, validated_data):
+        new_habit = Habit.objects.create(**validated_data)
+
+        if new_habit.duration > 120:
+            raise serializers.ValidationError("Duration cannot be more than 120 sec")
+
+        if new_habit.nice_habit is False:
+            if not new_habit.reward:
+                if not new_habit.linked_habit:
+                    raise serializers.ValidationError("Default habit must has reward or nice habit")
+
+            else:
+                if new_habit.linked_habit:
+                    raise serializers.ValidationError("Common habit cannot have linked habit and reward simultaneously")
+
+            return new_habit
+
+        else:
+            if new_habit.reward:
+                raise serializers.ValidationError("Nice habit cannot have linked habit or reward")
+            return new_habit
 
     class Meta:
         model = Habit
--- spa_app/tests.py
+++ spa_app/tests.py
@@ -1,3 +1,245 @@
-from django.test import TestCase
+from django.urls import reverse
+from rest_framework import status
+from rest_framework_simplejwt.tokens import AccessToken
+from rest_framework.test import APITestCase, APIClient
 
-# Create your tests here.
+from config.settings import AUTH_USER_MODEL
+
+from spa_app.models import Habit, FrequencyChoices
+from users.models import User
+
+
+class HabitTestCase(APITestCase):
+
+    def setUp(self):
+        self.user = User(
+            email='test@mail.ru',
+            tg_name='test',
+            is_active=True,
+            is_staff=False,
+            is_superuser=False
+        )
+        self.user.set_password('12345')
+        self.user.save()
+
+        self.client = APIClient()
+        token = AccessToken.for_user(user=self.user)
+        self.client.credentials(HTTP_AUTHORIZATION=f'Bearer {token}')
+
+        self.public_habit = Habit.objects.create(
+            place="TestPublic",
+            time="14:00:00",
+            action="TestPublic",
+            nice_habit="False",
+            frequency="SUNDAY",
+            reward="The bag of chips",
+            duration=10,
+            published="True",
+            user=self.user
+        )
+
+        self.private_habit = Habit.objects.create(
+            place="TestDefault",
+            time="14:00:00",
+            action="TestDefault",
+            nice_habit="False",
+            frequency="SUNDAY",
+            reward="The bag of chips",
+            duration=10,
+            published="False",
+            user=self.user
+        )
+
+    def test_create_habit(self):
+        data = {
+            "id": 5,
+            "place": "TestCreate",
+            "time": "14:00:00",
+            "action": "TestCreate",
+            "nice_habit": "False",
+            "frequency": "SUNDAY",
+            "reward": "The bag of chips",
+            "duration": 10,
+            "published": "True",
+            "user": self.user
+            }
+
+        url = reverse('spa_app:habit_create')  # Используйте правильное имя URL-адреса
+        response = self.client.post(url, data)
+
+        self.assertEqual(
+            response.status_code,
+            status.HTTP_201_CREATED
+        )
+
+        self.assertTrue(
+            Habit.objects.filter(place='TestCreate').exists()
+        )
+
+    def test_public_list_habit(self):
+        response = self.client.get('spa/habits/publish/')
+
+        self.assertEqual(
+            response.status_code,
+            status.HTTP_200_OK
+        )
+
+    def test_private_list_habit(self):
+        data = {
+            "id": 5,
+            "place": "TestCreate",
+            "time": "14:00:00",
+            "action": "TestCreate",
+            "nice_habit": "False",
+            "frequency": "SUNDAY",
+            "reward": "The bag of chips",
+            "duration": 10,
+            "published": "True",
+            "user": self.user
+            }
+
+        self.client.post(
+            "/spa/habits/create/",
+            data
+        )
+
+        response = self.client.get('spa/habits/my/')
+
+        self.assertEqual(
+            response.status_code,
+            status.HTTP_200_OK
+        )
+
+    def test_detail_habit(self):
+        data = {
+            "id": 5,
+            "place": "TestCreate",
+            "time": "14:00:00",
+            "action": "TestCreate",
+            "nice_habit": "False",
+            "frequency": "SUNDAY",
+            "reward": "The bag of chips",
+            "duration": 10,
+            "published": "True",
+            "user": self.user
+            }
+
+        url = reverse('spa_app:habit_create')  # Используйте правильное имя URL-адреса
+        self.client.post(url, data)
+
+        response = self.client.get('spa/habits/retrieve/1/')
+
+        self.assertEqual(
+            response.status_code,
+            status.HTTP_200_OK
+        )
+
+    def test_update_habit(self):
+        data = {
+            "place": "TestCreate",
+            "time": "14:00:00",
+            "action": "TestCreate",
+            "nice_habit": "False",
+            "frequency": "SUNDAY",
+            "reward": "The bag of chips",
+            "duration": "10",
+            "published": "True",
+            }
+
+        update_data = {
+            "place": "TestUpdate",
+            "action": "TestUpdate",
+            }
+
+        habit = Habit.objects.create(**data)
+
+        response = self.client.put(reverse('spa_app:habit_update', kwargs={'pk': habit.pk}), data=update_data)
+
+        self.assertEqual(
+            response.status_code,
+            status.HTTP_200_OK
+        )
+        self.assertTrue(
+            Habit.objects.filter(place='TestUpdate').exists()
+        )
+
+    def test_destroy_habit(self):
+        habit = Habit.objects.create(
+            id=2,
+            place='TestCreate',
+            action='TestCreate',
+            frequency='MONDAY',
+            reward='The bag of chips',
+            duration=20,
+            published=False,
+            user=self.user,
+        )
+
+        url = reverse('spa_app:habit_destroy', kwargs={'pk': habit.id})
+
+        response = self.client.delete(url)
+
+        self.assertEqual(
+            response.status_code,
+            status.HTTP_404_NOT_FOUND
+        )
+
+    def test_validate_duration(self):
+        data = {
+            'id': 2,
+            'place': 'TestCreate',
+            'action': 'TestCreate',
+            'frequency': 'MONDAY',
+            'reward': 'The bag of chips',
+            'duration': 121,
+            'published': False,
+            'user': self.user
+        }
+
+        response = self.client.post('spa/habits/create/', data)
+
+        self.assertEqual(
+            response.status_code,
+            status.HTTP_400_BAD_REQUEST
+        )
+
+    def test_validate_nice_habit(self):
+        data = {
+            'id': 2,
+            'place': 'TestCreate',
+            'action': 'TestCreate',
+            'nice_habit': True,
+            'frequency': 'MONDAY',
+            'reward': 'The bag of chips',
+            'duration': 10,
+            'published': False,
+            'user': self.user
+        }
+
+        response = self.client.post('spa/habits/create/', data)
+
+        self.assertEqual(
+            response.status_code,
+            status.HTTP_400_BAD_REQUEST
+        )
+
+    def test_validate_default_habit(self):
+        data = {
+            'id': 2,
+            'place': 'TestCreate',
+            'action': 'TestCreate',
+            'nice_habit': False,
+            'linked_habit': self.nice_habit.pk,
+            'frequency': 'MONDAY',
+            'reward': 'The bag of chips',
+            'duration': 10,
+            'published': False,
+            'user': self.user
+        }
+
+        response = self.client.post('spa/habits/create/', data)
+
+        self.assertEqual(
+            response.status_code,
+            status.HTTP_400_BAD_REQUEST
+        )
